9352b6176b4f6aa03c3d58d6d626ddc324c1e061
helemanc <elemanc@hotmail.it>
2023-05-05 19:05:39 +0200

    Added test code

---
+++ tests/gender/utils.py
@@ -0,0 +1,95 @@
+from voice_activity_detection import vad
+from speech_emotion_recognition import feature_extraction as fe, ensemble
+import scipy
+import numpy as np
+from scipy import signal
+from scipy.io.wavfile import write
+import datetime
+
+def denoise(samples):
+    """
+    :param samples: an array representing the sampled audio file
+    :type samples: float
+    :return: an array representing the clean audio file
+    :rtype: float
+    """
+    samples_wiener = scipy.signal.wiener(samples)
+    return samples_wiener
+
+
+def execute_vad_ser(segmenter, filepath, prediction_scheme):
+    """
+    :param segmenter: an instance of the Segmenter object of Ina Speech Segementer
+    :type segmenter: Segmenter object
+    :param filepath: a string representing the full-path of the input audio file
+    :type filepath: string
+    :param prediction_scheme: a string representing the aggregation strategy to be used in the ensemble
+    :type prediction_scheme: string
+
+    TO DO
+    :return: an integer flag representing the action to be sent to the control center
+    :rtype: int
+    """
+
+    samples, sample_rate = fe.read_file(filepath)
+    samples, sample_rate = resample(samples, sample_rate)
+    new_samples = fe.cut_pad(samples)
+    samples_wiener = denoise(new_samples)
+    new_filepath = "voice_activity_detection/tmp.wav"
+    write(new_filepath, sample_rate, samples_wiener)
+
+    seg = vad.check_speech(segmenter, new_filepath)
+    if seg == 'speech':
+        print("#########################################\n")
+        print("The audio contains speech. \nStarting Speech Emotion Recognition process.\n")
+        print("#########################################\n")
+
+        # start_time = datetime.datetime.now()
+
+        final_prediction = ensemble.ensemble(new_samples, prediction_scheme)
+
+        # elapsed = datetime.datetime.now() - start_time
+        # print("Time elapsed for SER", elapsed)
+
+        if final_prediction == 1:
+            print("Speech contains disruptive emotion.")
+        else:
+            print("Speech does not contain disruptive emotion.")
+
+def resample(input_data, sample_rate, required_sample_rate=16000, amplify=False):
+    """
+    Resampling function. Takes an audio with sample_rate and resamples
+    it to required_sample_rate. Optionally, amplifies volume.
+    ​
+    :param input_data: Input audio data
+    :type input_data: numpy.ndarray
+    :param sample_rate: Sample rate of input audio data
+    :type sample_rate: int
+    :param required_sample_rate: Sample rate to resample original audio, defaults to 16000
+    :type required_sample_rate: int, optional
+    :param amplify: Whether to amplify audio volume, defaults to False
+    :type amplify: bool, optional
+    :return: Resampled audio and new sample rate
+    :rtype: numpy.ndarray, int
+    """
+    if sample_rate < required_sample_rate:
+        resampling_factor = int(round(required_sample_rate/sample_rate, 0))
+        new_rate = sample_rate * resampling_factor
+        samples = len(input_data) * resampling_factor
+        resampled = signal.resample(input_data, samples)
+    elif sample_rate > required_sample_rate:
+        resampling_factor = int(round(sample_rate/required_sample_rate, 0))
+        new_rate = int(sample_rate / resampling_factor)
+        resampled = signal.decimate(input_data, resampling_factor)
+    else:
+        resampling_factor = 1
+        new_rate = sample_rate
+        resampled = input_data
+    if amplify and input_data.size > 0:
+        absolute_values = np.absolute(resampled)
+        max_value = np.amax(absolute_values)
+        max_range = np.iinfo(np.int16).max
+        amplify_factor = max_range/max_value
+        resampled = resampled * amplify_factor
+        resampled = resampled.round()
+    return resampled.astype(np.float32), new_rate
---
+++ tests/transfer_learning/feature_extraction.py
@@ -0,0 +1,205 @@
+import math
+import librosa
+import numpy as np
+import pickle
+import os
+
+LENGTH_CHOSEN = 80000
+#SCALERS_FOLDER = "speech_emotion_recognition/scalers/data_scaler"
+#SCALERS_FOLDER = "speech_emotion_recognition/scalers/data_scaler_ravdess"
+SCALERS_FOLDER = "scalers"
+def read_file(audio_file):
+    """
+
+    :param audio_file: a string representing the full-path of the input audio file
+    :type audio_file: string
+    :return: an array representing the sampled audio file, sample rate
+    :rtype: np.array, int
+    """
+    samples, sr = librosa.load(audio_file, res_type='kaiser_fast', sr=16000)
+    return samples, sr
+
+
+def cut_pad(samples):
+    """
+
+![](../../../Library/Group Containers/group.com.apple.notes/Accounts/LocalAccount/Media/45B5498C-5174-4A3E-8F62-C5D85AB24B47/Pasted Graphic.png)    :param samples: an array representing the sampled audio
+    :type samples: np.array, float64
+    :return: an array representing the cut or padded audio file
+    :rtype: np.array, float64
+    """
+    # cut
+    if samples.shape[0] > LENGTH_CHOSEN:
+        new_samples = samples[:LENGTH_CHOSEN]
+    # pad
+    elif samples.shape[0] < LENGTH_CHOSEN:
+        new_samples = np.pad(samples, math.ceil((LENGTH_CHOSEN - samples.shape[0]) / 2), mode='median')
+    # nothing
+    else:
+        new_samples = samples
+    if len(new_samples) == 80001:
+        new_samples = new_samples[:-1]
+    return new_samples
+
+
+def compute_energy(samples):
+    """
+
+    :param samples: an array representing the sampled audio
+    :type samples: float64
+    :return: a value representing the rms on the input audio
+    :rtype: float
+    """
+    energy = librosa.feature.rms(samples)
+    energy = energy.T
+    energy = np.array(energy)
+    return energy
+
+
+def compute_energy_mean(samples):
+    """
+
+
+    :param samples: an array representing the sampled audio
+    :type samples: float64
+    :return: a value representing the average rms on the input audio
+    :rtype: float
+    """
+    energy = librosa.feature.rms(samples)
+    energy = energy.T
+    energy = np.array(energy)
+    energy = np.mean(energy, axis=0)
+    return energy
+
+
+def compute_mfccs(samples, n_mfcc, scaler, feature_energy):
+    """
+
+    :param samples: an array representing the sampled audio
+    :type samples: np.array, float 64
+    :param n_mfcc: the desired number of mfcc
+    :type n_mfcc: int
+    :param scaler: the full-path of the scaler pickle object
+    :type scaler: string
+    :param feature_energy: a value to indicate whether the energy feature should be concatenated to MFCCs
+    :type feature_energy: boolean
+    :return: an of audio features
+    :rtype: np.array, float64
+    """
+    # Compute MFCCS
+    mfccs = librosa.feature.mfcc(y=samples, sr=16000, n_mfcc=n_mfcc)
+    mfccs = mfccs.T
+    mfccs = np.array(mfccs)
+    mfccs = mfccs[:, 1:]  # get rid of the first component
+
+    # Compute energy, if required
+    if feature_energy == True:
+        energy = compute_energy(samples)
+        features = []
+        conc = np.column_stack((mfccs, energy))
+        features.append(conc)
+        mfccs = np.array(features)
+
+    # Reshape features in (1, 157, n_mfcc)
+    if feature_energy == True:
+        mfccs = mfccs.reshape(1, 157, n_mfcc)
+    else:
+        mfccs = mfccs.reshape(1, 157, n_mfcc-1)
+
+    # Load scaler
+    SCALER_PATH = os.path.join(SCALERS_FOLDER, scaler)
+    # print("SCALER PATH", SCALER_PATH)
+    with open(SCALER_PATH, 'rb') as f:
+        scaler = pickle.load(f)
+
+    # print("Shape MFCCS", mfccs.shape)
+    # Scale data
+    mfccs = scaler.transform(mfccs.reshape(-1, mfccs.shape[-1])).reshape(mfccs.shape)
+
+    return mfccs
+
+
+def compute_mfccs_mean(samples, n_mfcc, scaler, feature_energy):
+    """
+
+    :param samples: an array representing the sampled audio
+    :type samples: np.array, float 64
+    :param n_mfcc: the desired number of mfcc
+    :type n_mfcc: int
+    :param scaler: the full-path of the scaler pickle object
+    :type scaler: string
+    :param feature_energy: a value to indicate whether the energy feature should be concatenated to MFCCs
+    :type feature_energy: boolean
+    :return: an of audio features
+    :rtype: np.array, float64
+    """
+    mfccs = librosa.feature.mfcc(y=samples, sr=16000, n_mfcc=n_mfcc)
+    mfccs = mfccs.T
+    mfccs = np.array(mfccs)
+    mfccs = np.mean(mfccs[:, 1:], axis=0)
+    # print("Shape MFCCS", mfccs.shape)
+
+    # Compute energy, if required
+    if feature_energy == True:
+        energy = compute_energy_mean(samples)
+        features = []
+        conc = np.concatenate((mfccs, energy), axis=None)
+        features.append(conc)
+        mfccs = np.array(features)
+
+    # Reshape features in (1, 157, n_mfcc)
+    if feature_energy == True:
+        mfccs = mfccs.reshape(1, n_mfcc)
+    else:
+        mfccs = mfccs.reshape(1, n_mfcc - 1)
+    # Load scaler
+    SCALER_PATH = os.path.join(SCALERS_FOLDER, scaler)
+    # print("SCALER PATH", SCALER_PATH)
+    with open(SCALER_PATH, 'rb') as f:
+        scaler = pickle.load(f)
+
+    # Scale data
+    mfccs = scaler.transform(mfccs.reshape(-1, mfccs.shape[-1])).reshape(mfccs.shape)
+
+    return mfccs
+
+
+def mfccs_scaled(samples, scaler, id_exp):
+    """
+
+    :param samples: an array representing the sampled audio
+    :type samples: np.array, float 64
+    :param scaler: the full-path of the scaler pickle object
+    :type scaler: string
+    :param id_exp: a string that identifies the experiment
+    :type id_exp: string
+    :return: an array of features
+    :rtype: np.array, float64
+    """
+    parts = id_exp.split('_')
+    num_exp = parts[0]
+    # print("Computing features for Experiment: ", num_exp)
+    if num_exp == '1':
+        mfccs = compute_mfccs(samples, 13, scaler, feature_energy=False)
+        return mfccs
+    elif num_exp == '2':
+        mfccs = compute_mfccs(samples, 13, scaler, feature_energy=True)
+        return mfccs
+    elif num_exp == '3':
+        mfccs = compute_mfccs(samples, 26, scaler, feature_energy=False)
+        return mfccs
+    elif num_exp == '4':
+        mfccs = compute_mfccs(samples, 26, scaler, feature_energy=True)
+        return mfccs
+    elif num_exp == '5':
+        mfccs = compute_mfccs_mean(samples, 13, scaler, feature_energy=False)
+        return mfccs
+    elif num_exp == '6':
+        mfccs = compute_mfccs_mean(samples, 13, scaler, feature_energy=True)
+        return mfccs
+    elif num_exp == '7':
+        mfccs = compute_mfccs_mean(samples, 26, scaler, feature_energy=False)
+        return mfccs
+    elif num_exp == '8':
+        mfccs = compute_mfccs_mean(samples, 26, scaler, feature_energy=True)
+        return mfccs
