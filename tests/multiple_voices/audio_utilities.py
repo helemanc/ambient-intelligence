d8b61b5cf150c429f52c851d3744d0dbc380b572
helemanc <elemanc@hotmail.it>
2023-05-16 01:25:37 +0200

    Added multiple voices tests

--- tests/multiple_voices/audio_utilities.py
+++ tests/multiple_voices/audio_utilities.py
@@ -5,32 +5,47 @@ from tqdm import tqdm
 import pandas as pd
 import numpy as npù
 
-def create_overlapped_files(dataset_name, df_test): 
+def create_overlapped_files(dataset_name, n, df_test): 
     project_dir = os.path.normpath(os.path.dirname(os.path.abspath(__file__)))
     emotion = df_test['emotion_label'][0]
-    for index, row in tqdm(df_test.iterrows()): 
-        path = row['path']
-
-        if index == 0: 
-            sound1 = AudioSegment.from_file(path)
-            samples, sr = librosa.load(path, res_type='kaiser_fast', sr=16000)
-            duration = librosa.get_duration(y = samples, sr = sr)
-            sound2 = AudioSegment.from_file(df_test.iloc[index+1]['path'])
-            combined = sound1.overlay(sound2)
-        else:
-            sound = AudioSegment.from_file(path)
-            combined = combined.overlay(sound)
-
-    n_overlapped_files = len(df_test)
-    # create a folder named "dataset_name" and a subfolder named "overlapped_" + n_overlapped_files if not exist
-    path_overlapped = os.path.join(project_dir, "overlapped_audio_files", dataset_name, "overlapped_" + str(n_overlapped_files))
-    if not os.path.exists(path_overlapped):
-        os.makedirs(path_overlapped)
+    emotions = []
+    paths = []
+    # cut the len(df_test)%n samples to have a multiple of n
+    if len(df_test)%n != 0:
+        df_test = df_test.iloc[:-(len(df_test)%n)]
     
-    # save new_path 
-    new_path = os.path.join(path_overlapped, "overlapped_audio_" + str(emotion) + ".wav")
-    combined.export(new_path, format= 'wav')
-    df_test = pd.DataFrame([emotion, new_path]).T
+    file_index = 1 
+    # create overlapped files
+    for i in tqdm(range(0, len(df_test), n)):
+
+        # overlap n files 
+        for k in range(n):
+            path = df_test.iloc[i+k]['path']
+            if k == 0: 
+                sound1 = AudioSegment.from_file(path)
+                samples, sr = librosa.load(path, res_type='kaiser_fast', sr=16000)
+                duration = librosa.get_duration(y = samples, sr = sr)
+                sound2 = AudioSegment.from_file(df_test.iloc[i+k+1]['path'])
+                combined = sound1.overlay(sound2)
+            else:
+                sound = AudioSegment.from_file(path)
+                combined = combined.overlay(sound)
+       
+        n_overlapped_files = n
+
+        # create a folder named "dataset_name" and a subfolder named "overlapped_" + n_overlapped_files if not exist
+        path_overlapped = os.path.join(project_dir, "overlapped_audio_files", dataset_name, "overlapped_" + str(n_overlapped_files))
+        if not os.path.exists(path_overlapped):
+            os.makedirs(path_overlapped)
+    
+        # save audio file at new_path 
+        new_path = os.path.join(path_overlapped, "overlapped_audio_" + str(file_index) + '_' + str(emotion) + ".wav")
+        combined.export(new_path, format= 'wav')
+        emotions.append(emotion)
+        paths.append(new_path)
+        file_index += 1
+
+    df_test = pd.DataFrame([emotions, paths]).T
     df_test.columns = ['emotion_label', 'path']
 
     
--- tests/multiple_voices/main.py
+++ tests/multiple_voices/main.py
@@ -1,6 +1,7 @@
 from utilities import *
 import os 
 from audio_utilities import *
+from sklearn.utils import shuffle
 
 N = [2, 5, 10]
 RAV_train, RAV_val, RAV_test = extract_ravdess(gender=False)
@@ -8,10 +9,11 @@ SAVEE_train, SAVEE_val, SAVEE_test = extract_savee() # test male
 TESS_train, TESS_test = extract_tess() # test female 
 CREMA_train, CREMA_val, CREMA_test = extract_crema(gender=False)
 
-RAV_model_list = ['3_2', '3_1', '4_1', '7_1', '7_2', '8_2']
-TESS_model_list = ['3_3', '3_4', '4_3', '5_4', '8_4', '5_3']
-SAVEE_model_list = ['1_6', '3_5', '4_6', '8_5', '5_6', '6_5']
-CREMA_model_list = ['2_8', '1_8', '3_7', '7_8', '8_8', '6_8']
+# only best models 
+RAV_model_list = ['3_2'] #, '3_1', '4_1', '7_1', '7_2', '8_2']
+TESS_model_list = ['3_3'] #, '3_4', '4_3', '5_4', '8_4', '5_3']
+SAVEE_model_list = ['1_6']# , '3_5', '4_6', '8_5', '5_6', '6_5']
+CREMA_model_list = ['2_8'] #, '1_8', '3_7', '7_8', '8_8', '6_8']
 
 best_models_list = RAV_model_list + SAVEE_model_list + TESS_model_list + CREMA_model_list
 test_sets_list = [RAV_test, SAVEE_test, TESS_test, CREMA_test]
@@ -31,13 +33,20 @@ for i, df_test in enumerate(test_sets_list):
         df_test_pos = df_test_pos.reset_index(drop=True)
 
         # random pick 10 samples marked as 1 and 10 samples marked as 0
-        df_test_neg = df_test_neg.sample(n=n, random_state=42).reset_index(drop=True)
-        df_test_pos = df_test_pos.sample(n=n, random_state=42).reset_index(drop=True)
+        # shuffle df_test_neg and df_test_pos
+        df_test_neg = shuffle(df_test_neg, random_state=42)
+        df_test_pos = shuffle(df_test_pos, random_state=42)
+
+        #print(test_sets_name[i], n, len(df_test_neg), len(df_test_neg)%n)
+        #print(test_sets_name[i], n, len(df_test_pos), len(df_test_pos)%n)
+
+        #df_test_neg = df_test_neg.sample(n=n, random_state=42).reset_index(drop=True)
+        #df_test_pos = df_test_pos.sample(n=n, random_state=42).reset_index(drop=True)
 
 
 
-        df_test_neg = create_overlapped_files(test_sets_name[i], df_test_neg)
-        df_test_pos = create_overlapped_files(test_sets_name[i], df_test_pos)
+        df_test_neg = create_overlapped_files(test_sets_name[i], n,  df_test_neg)
+        df_test_pos = create_overlapped_files(test_sets_name[i], n,  df_test_pos)
         # overlap audio file of df_test_neg and df_test_pos located at path feature
         if test_sets_name[i] == 'RAVDESS':
             model_list = RAV_model_list
@@ -48,8 +57,10 @@ for i, df_test in enumerate(test_sets_list):
         elif test_sets_name[i] == 'CREMA':
             model_list = CREMA_model_list
 
-        make_test(df_test_neg, model_list, test_sets_name[i] + '_NEG_' + str(n))
-        make_test(df_test_pos, model_list, test_sets_name[i] + '_POS_' + str(n))
+        df = pd.concat([df_test_neg, df_test_pos]).reset_index(drop=True)
+        make_test(df, model_list, test_sets_name[i] + '_GENERAL_' + str(n))
+        #make_test(df_test_neg, model_list, test_sets_name[i] + '_NEG_' + str(n))
+        #make_test(df_test_pos, model_list, test_sets_name[i] + '_POS_' + str(n))
 
 # make_test(RAV_test_m, RAV_model_list, 'ravdess_m')
 # make_test(SAVEE_test, SAVEE_model_list, 'savee_m')
